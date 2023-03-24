305ab4d0cd7f6a87f1737033097aa6b323b20a9b
Tebogo-vee <tebogomakgato19@gmail.com>
2023-03-24 13:12:05 -0700

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,10 +15,10 @@ int maini(void)
 	{
 		if ( i % 3 == 0 && i % 5 != 0)
 		{
-			printf(" Fizz");
+			printf("Fizz");
 		} else if (i % 5 == 0 && i % 3 !=0)
 		{
-			printf(" Buzz");
+			printf("Buzz");
 		} else if (i % 3 == 0 && i  % 5 == 0)
 		{
 			printf( "FizzBuzz");

aeceb723b7b1a356c42e4b05b2827d657385b91a
Tebogo-vee <tebogomakgato19@gmail.com>
2023-03-27 09:35:58 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string 
+ * Return: lenth
+ */
+int _strlen(char *s)
+{
+	int longe = 0;
+	
+       while (*s != '\0')
+       {
+       longe++;
+       s++;
+       }
+
+       return (longe);
+}

4061a49518c2d04079095e59ec96a819f5991c5b
Tebogo-vee <tebogomakgato19@gmail.com>
2023-03-30 12:03:29 -0700

    102-magic.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,24 +1,25 @@
 #include "main.h"
 
-
 /**
  * print_number - Prints an integer.
  * @: The integer to be printed 
- *
+ * Return: 0
  */
 
 void print_number(int n)
-{
 
-	unsigned int numb = n;
+{
+	unsigned int numb = n
 
 	if (n < 0)
 	{
 	_putchar('-');
-	numb = -numb;
+	numb = -n;
 	}
 
 	if ((numb / 10) > 0)
+	{
 	print_number(numb / 10);
+	}
 	_putchar((numb % 10) + '0');
 }

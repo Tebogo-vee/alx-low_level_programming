b228b44050ecebf85aa059a9bbb7528b73bcecd9
Tebogo-vee <tebogomakgato19@gmail.com>
2023-04-06 14:32:05 -0700

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen_recursion - Returns the length of a string 
+ * @s: The string to count
+ *
+ * Return: Intiger
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+
+		return (0);
+
+	s++;
+	return (_strlen_recursion(s) + 1);
+
+}
+

537978a1ec0d422c51f8eedc7644bfb80cd3e8c1
Tebogo-vee <tebogomakgato19@gmail.com>
2023-05-03 12:34:30 -0700

    1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a linked list_t lis
+ * @h:pointer to the list_t list
+ *
+ * Return::number of elements in h
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t p = 0;
+
+	while (h)
+	{
+		p++;
+		h = h->next;
+	}
+	return (p);
+}
